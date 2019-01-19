36927b83f3637eb027e7025ad4193bf8c680366f
Arjun-tp <arjuntpnambiar@gmail.com>
2019-01-19 17:09:49 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict'
 let req = require("express");
 let express = req();
+let fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 let config = require('./config/development');
 let db = config.db;
+let es = require('event-stream');
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
@@ -13,51 +14,60 @@ const dbName = config.db.mongo.db;
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function (err, client) {
-    assert.equal(null, err);
     console.log("Connected successfully to server - DB =>" + dbName);
 
     const db = client.db(dbName);
 
     const csvFilePath = 'cars.csv'
-    const csvtoJsonHere = require('csvtojson')
+    // const csvtoJsonHere = require('csvtojson')
 
-    async function csvtoJsonFunction (){
-        csvtoJsonHere()
-        .fromFile(csvFilePath)
-        .then((jsonObj) => {
-            console.log(JSON.stringify(jsonObj));
-            _.chunk(jsonObj, 5, (data) => {
-                let functionCallMade =  await insertCollection();
-                console.log("functionCallMade====",functionCallMade.length)
-                // console.log("functionCallMade====",functionCallMade)
-            })
+     function csvtoJsonFunction (){
+
+        fs.createReadStream(csvFilePath)
+        .pipe(es.split(/\n/))
+
+        .pipe(es.stringify())
+        .pipe(es.writeArray(function(err,dataArray){
+            console.log("dataArray========",dataArray)
         })
+        .on('error', function(err){
+            console.log('Error while reading file.', err);
+        })
+        .on('end', function(){
+            console.log('Read entire file.')
+        })
+    
+
+        )
+    
+    
+    
     }
 
 
-    (async () => {
-       await csvtoJsonFunction()
-      })();
 
 
-      function insertCollection (){
-        return new Promise(function(resolve,reject){
-            try {
-                const collection = db.collection('cars');
-                collection.insertMany(data, function (err, result) {
-                    console.log("Inserted into the collection", result);
-                    resolve(result);
-                });
-            }catch(e){
-                console.log("error",e);
-                reject(e);
-            }
-        })
+    // let csv = require('csv-parser');
+    //     let dataArray = [];
+    //     let header;
+    //     fs.createReadStream(csvFilePath)
+    //     .pipe(csv())
+    //     .on('data', function(data) {
+    //             //    header = data.split(/\n/)[0];
+    //         try {
+    //             dataArray.push(data); 
+    //         }
+    //         catch(err) {
+    //             console.log("err",JSON.stringify(err));
+    //         }
+    //     })
+    //     console.log("Array Length",dataArray.length)
         
-    }
 
+      csvtoJsonFunction();
+
+ 
     
 });
-// const jsonArray= csvtoJsonHere().fromFile(csvFilePath);
 
 
